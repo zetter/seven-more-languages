28979e179d48cb0be5fa7de5f92f2d29039f030e
Zetter <zetter@gmail.com>
2014-07-17 17:37:57 +0100

    print_primes

--- 1-lua/day-1/primes.lua
+++ 1-lua/day-1/primes.lua
@@ -27,3 +27,21 @@ print(is_prime(17))
 
 print('is_prime(18)')
 print(is_prime(18))
+
+function print_primes(num)
+  local primes_printed = 0
+  local last_number_checked = 0
+  while primes_printed < num do
+    repeat
+      last_number_checked = last_number_checked + 1
+    until is_prime(last_number_checked)
+    print(last_number_checked)
+    primes_printed = primes_printed + 1
+  end
+end
+
+print('print_primes(3)')
+print_primes(3)
+
+print('print_primes(20)')
+print_primes(20)
