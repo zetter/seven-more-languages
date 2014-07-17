260d82fdae1789cb9073abbb08ae8aa2a558840d
Zetter <zetter@gmail.com>
2014-07-17 17:13:20 +0100

    is_even

---
+++ 1-lua/day-1/is_even.lua
@@ -0,0 +1,15 @@
+function is_even(num)
+  return (num % 2) == 0
+end
+
+print('is_even(1)')
+print(is_even(1))
+
+print('is_even(2)')
+print(is_even(2))
+
+print('is_even(111)')
+print(is_even(111))
+
+print('is_even(112)')
+print(is_even(112))
