ad4ce8f931aa3a6d448fbfef20688750b9174701
Zetter <zetter@gmail.com>
2014-07-17 18:05:01 +0100

    Recursive version of for_loop

--- 1_lua/day_1/3_for_loop.lua
+++ 1_lua/day_1/3_for_loop.lua
@@ -10,3 +10,18 @@ for_loop(1,2,(function() print('hi') end))
 
 print("for_loop(2,6,(function() print('hi') end)")
 for_loop(2,6,(function() print('hi') end))
+
+function for_loop_recursive(a,b,f)
+  if a > b then
+    return
+  else
+    f()
+    for_loop_recursive(a + 1, b, f)
+  end
+end
+
+print("for_loop_recursive(1,2,(function() print('hi') end)")
+for_loop_recursive(1,2,(function() print('hi') end))
+
+print("for_loop_recursive(2,6,(function() print('hi') end)")
+for_loop_recursive(2,6,(function() print('hi') end))
